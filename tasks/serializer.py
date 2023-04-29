ceefd5ab0f337bed47d0def34a8f19c9f327fd9f
Matias Ramirez <matiaramirez133@gmail.com>
2023-04-29 17:35:14 -0300

    0002 tasks.py removed, changes to manage.py, apps.py, models.py, serializer.py, urls.py, views.py

--- tasks/serializer.py
+++ tasks/serializer.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import Task
 
+
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        #fiels = ('id', 'title', 'description', 'done')
+        # fields = ('id', 'title', 'description', 'done')
         fields = '__all__'
